43814314828ae0e09d05ffd35e23a1f25a66bd73
Waqar Muneer <i.do.dev@outlook.com>
2025-10-01 14:23:30 +0500

    define Unit Of Work dependency function

---
+++ api/dependencies.py
@@ -0,0 +1,7 @@
+from api.data.uow import UnitOfWork, uow_context
+from typing import AsyncGenerator
+
+# Unit of Work dependency to perform DB operations within a transaction
+async def get_uow() -> AsyncGenerator[UnitOfWork, None]:
+    async with uow_context() as uow:
+        yield uow
